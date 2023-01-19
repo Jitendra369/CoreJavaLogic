ded86da225b94ac91863c443a4cc6481929a8472
jittu <onekb.twotb@gmail.com>
2023-01-19 10:15:59 +0530

    upload update

--- src/com/hackerank/string/Palidrom.java
+++ src/com/hackerank/string/Palidrom.java
@@ -2,16 +2,42 @@ package com.hackerank.string;
 
 public class Palidrom {
     public static void main(String[] args) {
-        String strOdd = "maadam";
-        String strEven ="naam";
+        String str = "madam";
+//        String str = "java";
 
-        boolean result  = checkPalidriom(strOdd);
-        if (result == true){
-            System.out.println("String is palidrom");
-        }else{
-            System.out.println("String is not a palidrom");
-        }
+//        boolean result  = checkPalidriom(strOdd);
+//        if (result == true){
+//            System.out.println("String is palidrom");
+//        }else{
+//            System.out.println("String is not a palidrom");
+//        }
+
+          String isPalidrom =getReverseUsingSringBuilder(str);
+//        System.out.println(isPalidrom);
+
+        String result  = reverseString(str);
+        System.out.println(result);
     }
+
+    private static String reverseString(String str) {
+
+        String rev ="";
+        for (int i= str.length()-1;i>=0;i--){
+            rev = rev + String.valueOf(str.charAt(i));
+        }
+        if (str.equalsIgnoreCase(rev.trim())){
+            return "Yes";
+        }
+        return "No";
+    }
+
+    private static String getReverseUsingSringBuilder(String str) {
+        if (str.equals(new StringBuilder(str).reverse().toString())){
+            return "Yes";
+        }
+        return "No";
+    }
+
 
     private static boolean checkPalidriom(String strOdd) {
 
